269563d1e1f233b0afe08fadd851c6d201ff6fc2
dejanjaredic <dekijaredic@gmail.com>
2019-05-31 14:44:30 +0200

    Prepravljeno

--- aspnet-core/src/TestProject.Application/Services/Employe/EmployeService.cs
+++ aspnet-core/src/TestProject.Application/Services/Employe/EmployeService.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Abp.AutoMapper;
 using Abp.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using TestProject.Services.Experience;
 
 namespace TestProject.Services.Employe
 {
     public class EmployeService : TestProjectAppServiceBase, IEmployeService
     {
         private readonly IRepository<Models.Employe> _employeRepository;
+        private readonly IRepository<Models.Experience> _expRepository;
 
-        public EmployeService(IRepository<Models.Employe> employeRepository)
+        public EmployeService(IRepository<Models.Employe> employeRepository, IRepository<Models.Experience> expRepository)
         {
             _employeRepository = employeRepository;
+            _expRepository = expRepository;
         }
         public void Create(EmloyeDto input)
         {
@@ -24,6 +29,27 @@ namespace TestProject.Services.Employe
         {
             var employe = _employeRepository.Get(id);
             return employe.MapTo<EmloyeDto>();
+        }
+
+        public List<ExperienceDto> Details(int id, int lid)
+        {
+            var details = _expRepository.GetAll().Include(x => x.Employe)
+                .Where(y => y.EmployeId == id && y.LanguageId == lid).ToList();
+
+            var list = new List<ExperienceDto>();
+
+            foreach (var detail in details)
+            {
+                 var exp = new ExperienceDto();
+                exp.StartDate = detail.StartDate;
+                exp.EndDate = detail.EndDate;
+                exp.Description = detail.Description;
+                exp.Organisation = detail.Organisation;
+
+                list.Add(exp);
+            }
+
+            return ObjectMapper.Map<List<ExperienceDto>>(list);
         }
     }
 }
---
+++ aspnet-core/src/TestProject.Application/Services/Employe/EmployeeDetailsDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Abp.AutoMapper;
+using TestProject.Services.Experience;
+
+namespace TestProject.Services.Employe
+{
+    [AutoMap(typeof(Models.Employe))]
+    public class EmployeeDetailsDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public List<ExperienceDto> Experiences { get; set; } = new List<ExperienceDto>();
+    }
+}
